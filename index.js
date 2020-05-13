4925a503260c7f05f82471b5384292db2761967f
Mathias Buus <mathiasbuus@gmail.com>
2020-05-13 21:07:16 +0200

    use blake2b-universal

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const sodium = require('sodium-universal')
+const blake2b = require('blake2b-universal')
 
 module.exports = derive
 
 function derive (ns, masterKey, input, output) {
   if (!output) output = Buffer.alloc(32)
 
-  sodium.crypto_generichash_batch(output, [
+  blake2b.batch(output, [
     Buffer.from(Buffer.byteLength(ns, 'ascii') + '\n' + ns, 'ascii'),
     Buffer.isBuffer(input) ? input : Buffer.from(input)
   ], masterKey)
